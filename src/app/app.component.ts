ef6977bb89b819e35d2c9e75ed59736b31b347a8
Dmytro Les <dmitriy.s.les@gmail.com>
2016-09-28 17:51:28 +0300

    icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { MdIconRegistry } from '@angular2-material/icon';
 
 @Component({
   selector: 'app-root',
   encapsulation: ViewEncapsulation.None,
+  viewProviders: [MdIconRegistry],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
